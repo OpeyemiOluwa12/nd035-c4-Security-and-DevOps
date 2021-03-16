8fa8350dbce47fc8a5d4eada6e152fb1d9ef3294
OpeyemiOluwa <idrisbalikisopeyemi@gmail.com>
2021-03-16 18:48:13 +0100

    Add jwt Authentication filters

--- starter_code/src/main/java/com/example/demo/controllers/UserController.java
+++ starter_code/src/main/java/com/example/demo/controllers/UserController.java
@@ -7,17 +7,22 @@ import com.example.demo.model.persistence.repositories.UserRepository;
 import com.example.demo.model.requests.CreateUserRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
 
+
     @Autowired
     private UserRepository userRepository;
 
     @Autowired
     private CartRepository cartRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @GetMapping("/id/{id}")
     public ResponseEntity<User> findById(@PathVariable Long id) {
@@ -35,8 +40,16 @@ public class UserController {
         User user = new User();
         user.setUsername(createUserRequest.getUsername());
         Cart cart = new Cart();
+
         cartRepository.save(cart);
         user.setCart(cart);
+        if (createUserRequest.getPassword().length() < 7 ||
+                !createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())) {
+            //System.out.println("Error - Either length is less than 7 or pass and conf pass do not match. Unable to create ",
+            //		createUserRequest.getUsername());
+            return ResponseEntity.badRequest().build();
+        }
+        user.setPassword(bCryptPasswordEncoder.encode(createUserRequest.getPassword()));
         userRepository.save(user);
         return ResponseEntity.ok(user);
     }
--- starter_code/src/main/java/com/example/demo/model/persistence/User.java
+++ starter_code/src/main/java/com/example/demo/model/persistence/User.java
@@ -19,6 +19,11 @@ public class User {
     @JsonProperty
     private String username;
 
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Column(nullable = false)
+    private String password;
+
+
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "cart_id", referencedColumnName = "id")
     @JsonIgnore
@@ -46,6 +51,14 @@ public class User {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
 
